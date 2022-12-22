31988f96ac6afd5be5df66499f7ef207ab6bdc3d
Joe Lee <j1yl@pm.me>
2022-12-21 21:30:30 -0800

    final

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # cmail
+
 email sender with personalized messages
+
+## instructions
+
+- enable 2fa on google account
+- get app password from google account
+- replace user_email in main.py with your email
+- replace app_pass in main.py with your app password
+- install dependencies
+
+## usage
+
+```
+pip install pandas
+pip install yagmail
+pip install premailer
+python main.py
+```
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pandas as pd
+import yagmail
+import random
+import time
+
+user_email = "YOUREMAILHERE"
+app_pass = "APP_PASSWORD"
+SUBJECT_HERE = "YOUREMAILSUBJECTHERE"
+ATTACHMENT_NAME = "resume.txt"
+# EDIT EMAIL MESSAGE ON LINE 34
 
 
 def read_csv(filename: str):
@@ -14,15 +23,26 @@ def process_csv(df: object):
 
     i = 0
     while i < len(emails):
-      # do something
-      # send_email(emails[i], first_names[i], last_names[i], companies[i])
-        print(f"{first_names[i]} {last_names[i]}, {emails[i]}, {companies[i]}")
+        send_email(emails[i], first_names[i], last_names[i], companies[i])
         i = i + 1
 
 
-def send_email():
-    return
+def send_email(email: str, first_name: str, last_name: str, company: str):
+    time.sleep(random.randint(1, 5))
+    subject = SUBJECT_HERE
+    content = [f"""
+    Dear {first_name} {last_name} from {company},
+    """, ATTACHMENT_NAME]
+
+    with yagmail.SMTP(user_email, app_pass) as yag:
+        yag.send(email, subject, content)
+
+    print(f"Email sent to {email}, {first_name} {last_name} from {company}")
 
 
 def main():
-    return
+    data = read_csv("data.csv")
+    process_csv(data)
+
+
+main()
